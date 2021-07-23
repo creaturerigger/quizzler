e2f83b5ead70fa8a86dd26b63c52fb7f92087a9d
VOLKAN BAKIR <volkan_bakir@hotmail.com>
2021-07-24 00:41:33 +0300

    new files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'Question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,11 +29,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<Question> questions = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
 
   int indexOfQuestion = 0;
 
@@ -47,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[indexOfQuestion].questionText,
+                quizBrain.questions[indexOfQuestion].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,6 +70,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questions[indexOfQuestion].questionAnswer;
+                if (correctAnswer) {
+                  print('User got the right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
                   if (indexOfQuestion < 2) {
                     ++indexOfQuestion;
@@ -100,6 +103,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questions[indexOfQuestion].questionAnswer;
+                if (!correctAnswer) {
+                  print('User got the right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
                   if (indexOfQuestion < 2) {
                     ++indexOfQuestion;
